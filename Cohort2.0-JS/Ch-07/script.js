b36142f5ba39cded5ad0826ed33f7ac115337b9a
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-11-30 15:41:54 +0530

    Lecture Completed

---
+++ Cohort2.0-JS/Ch-07/script.js
@@ -0,0 +1,52 @@
+// Loops and Conditionals in Javascript : 
+
+// console.log('hello');
+
+let a = 12;
+
+if(a>10){
+    console.log("hello");
+}else{
+    console.log("byebye");
+}
+
+// truthy and falsy values : 
+
+// 0 , '' false null NaN undefined document.all -> falsy values 
+
+// if , else if , else sab easy hai 
+
+// condition ? true : false 
+
+12>13 ? console.log('yo ') : console.log('no');
+
+switch(1){
+    case 1 :
+        console.log('yes');
+        break;
+    case 2 :
+        console.log('yess');
+    default:
+        console.log('NNNN')
+}
+
+// loop ke use cases mein 2 prakar hein : 
+
+// Straightforward loops :-> na value badalti na printing badalti 
+
+// Dynamic loops :-> value badal skti hai and printing bhi badal skti hai 
+
+// for loop  :
+
+for(let i=0 ; i<10 ; i++){
+   console.log("harsh");
+}
+
+for(let i=0;i<49;i++){
+    console.log('Amitanshu Varshney')
+}
+
+for(let i=30;i>=10;i--){
+    console.log(i)
+}
+
